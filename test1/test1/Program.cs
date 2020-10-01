0bcc846b79cc4d1bbe55850cc5843ea28245bfbe
marinapaya <71770687+marinapaya@users.noreply.github.com>
2020-10-01 15:46:06 +0200

    commit Utils
    commit of new class utils

--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace test1
 {
@@ -20,6 +21,16 @@ namespace test1
 
             s3 = GeometrySolve.GetVectorLength3(1.0, 1.0, 1.0);
             System.Console.WriteLine("La longitud del vector en 3D es " + s3);
+
+            int s4;
+
+            s4 = utils.GetMinValue(3, 1);
+            System.Console.WriteLine("El valor menor es " + s4);
+
+            int s5;
+            
+            s5 = utils.GetMaxValue(0, 5);
+            System.Console.WriteLine("El valor mayor es " + s5);
         }
     }
 }
---
+++ test1/test1/utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test1
+{
+    class utils
+    {
+        public static int GetMinValue(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        public static int GetMaxValue(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+}

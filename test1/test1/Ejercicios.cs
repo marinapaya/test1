84abfb00b063b4d28278f1c83711e654a965f509
marinapaya <71770687+marinapaya@users.noreply.github.com>
2020-10-13 19:56:43 +0200

    comming ejercicios
    nuevos ejercicios

--- test1/test1/Ejercicios.cs
+++ test1/test1/Ejercicios.cs
@@ -32,6 +32,7 @@
             }
         }
 
+        //Ejercicio 3 primo o no
         public static void Ejercicio3(int numerico)
         {
             bool es_primo = utils.IsPrime(numerico);
@@ -42,5 +43,97 @@
                 System.Console.WriteLine("El numero " + numerico + " NO es primo");  
         }
 
+        //imprimir por pantalla x numeros
+        public static void Ejercicio4()
+        {
+            int i;
+            i = 0;
+            while(i <= 100 )
+            {
+                System.Console.WriteLine(i);
+                i++;
+            }
+        }
+
+        public static void Ejercicio5()
+        {
+            int i;
+            i = 0;
+            while (i <= 100)
+            {
+                System.Console.WriteLine(i * 3);
+                i++;
+            }
+        }
+        public static void Ejercicio6()
+        {
+            int i;
+            i = 0;
+            while (i <= 100)
+            {
+                System.Console.WriteLine(i * i);
+                i++;
+            }
+        }
+        public static void Ejercicio7()
+        {
+            int i;
+            i = 0;
+            while (i <= 100)
+            {
+                System.Console.WriteLine(i * i + 1);
+                i++;
+            }
+        }
+
+        public static void Ejercicio8()
+        {
+            int i;
+            i = 0;
+            while (i <= 100)
+            {
+                System.Console.WriteLine(5 - i);
+                i++;
+            }
+        }
+
+        public static void Ejercicio9()
+        {
+            int i = 0;
+            while (i <= 100)
+            {
+                //if(i % 2 == 0)
+                if(utils.IsEven(i))
+                    System.Console.WriteLine(i);       
+                else
+                    System.Console.WriteLine(-i);
+                i++;
+            }
+            
+            
+        }
+
+        public static void Ejercicio10()
+        {
+            //esta variable muestra el anteanterior
+            int aa = 0;
+            int a = 1;
+            int i = 0;
+            int aux;
+            System.Console.WriteLine(aa);
+            System.Console.WriteLine(a);
+            
+            while (i <= 100)
+            {
+                System.Console.WriteLine(a + aa);
+                aux = a;
+                a = aa + a;
+                aa = aux;
+
+                i++;
+            }
+
+
+        }
     }
 }
--- test1/test1/Program.cs
+++ test1/test1/Program.cs
@@ -7,50 +7,66 @@ namespace test1
     {
         static void Main(string[] args)
         {
-            double s1;
+            // double s1;
 
-            s1 = EquationSolve.SolveEquation(0.1, 1.0, 2.0);
-            System.Console.WriteLine("s1 es" + s1);
+            //s1 = EquationSolve.SolveEquation(0.1, 1.0, 2.0);
+            //System.Console.WriteLine("s1 es" + s1);
 
-            double s2;
+            // double s2;
 
-            s2 = GeometrySolve.GetVectorLength2(1.0, 1.0);
-            System.Console.WriteLine("La longitud del vector en 2D es " + s2);
+            // s2 = GeometrySolve.GetVectorLength2(1.0, 1.0);
+            // System.Console.WriteLine("La longitud del vector en 2D es " + s2);
 
-            double s3;
+            // double s3;
 
-            s3 = GeometrySolve.GetVectorLength3(1.0, 1.0, 1.0);
-            System.Console.WriteLine("La longitud del vector en 3D es " + s3);
+            //s3 = GeometrySolve.GetVectorLength3(1.0, 1.0, 1.0);
+            // System.Console.WriteLine("La longitud del vector en 3D es " + s3);
 
-            int s4;
+            // int s4;
 
-            s4 = utils.GetMinValue(3, 1);
-            System.Console.WriteLine("El valor menor es " + s4);
+            // s4 = utils.GetMinValue(3, 1);
+            // System.Console.WriteLine("El valor menor es " + s4);
 
-            int s5;
-            
-            s5 = utils.GetMaxValue(0, 5);
-            System.Console.WriteLine("El valor mayor es " + s5);
+            // int s5;
 
-            bool s6;
+            // s5 = utils.GetMaxValue(0, 5);
+            // System.Console.WriteLine("El valor mayor es " + s5);
 
-            s6 = utils.IsEven(6);
-            System.Console.WriteLine("¿Es s6 par? " + s6);
+            // bool s6;
 
-            Ejercicios.Ejercicio1();
+            //s6 = utils.IsEven(6);
+            //System.Console.WriteLine("¿Es s6 par? " + s6);
 
-            Ejercicios.Ejercicio2(10);
+            // Ejercicios.Ejercicio1();
+
+            //Ejercicios.Ejercicio2(10);
 
             //Ejercicios.Ejercicio3(1);
             //Ejercicios.Ejercicio3(2);
 
-            int i;
-            i = 2;
-            while (i <= 10)
-            {
-                Ejercicios.Ejercicio3(i);
-                i++;
-            }
+            // int i;
+            // i = 2;
+            // while (i <= 10)
+            // {
+            // Ejercicios.Ejercicio3(i);
+            // i++;
+            //}
+
+            //Ejercicios.Ejercicio4();
+
+            // Ejercicios.Ejercicio5();
+
+            // Ejercicios.Ejercicio6();
+
+            // Ejercicios.Ejercicio7();
+
+            // Ejercicios.Ejercicio8();
+
+            // Ejercicios.Ejercicio9();
+
+            Ejercicios.Ejercicio10();
+
+
         }
     }
 }
